b2ce134581c2145b35c5d1235d4b144146195427
marcosrebelo97 <mgr@icomp.ufam.edu.br>
2023-08-04 11:12:10 -0400

    Implementando class UserDTO

--- src/main/java/com/exemplo/starterkit/api/dto/UserDTO.java
+++ src/main/java/com/exemplo/starterkit/api/dto/UserDTO.java
@@ -1,5 +1,6 @@
 package com.exemplo.starterkit.api.dto;
 
+import com.exemplo.starterkit.domain.model.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,11 +12,19 @@ import lombok.Setter;
 @NoArgsConstructor
 public class UserDTO {
 
-    private Long id;
+    //private Long id;
     private String name;
     private Integer age;
     private String email;
     private String city;
     private Integer cep;
+
+    public UserDTO(User user){
+        name = user.getName();
+        age = user.getAge();
+        email = user.getEmail();
+        city = user.getCity();
+        cep = user.getCep();
+    }
 
 }
