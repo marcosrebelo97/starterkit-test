2b899bee7e6def7d73507907b633da091a198b2a
marcosrebelo97 <mgr@icomp.ufam.edu.br>
2023-08-09 18:07:56 -0400

    Test class principal ok

--- src/test/java/com/exemplo/starterkit/StarterKitApplicationTests.java
+++ src/test/java/com/exemplo/starterkit/StarterKitApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class StarterKitApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		StarterKitApplication.main(new String[] {});
 	}
 
 }
