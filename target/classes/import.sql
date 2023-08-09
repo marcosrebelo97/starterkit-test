ad8e635d00697d09c1ebde6bb9dc48c6b428efa6
marcosrebelo97 <mgr@icomp.ufam.edu.br>
2023-08-09 11:11:16 -0400

    Remover pasta do rastreamento

--- target/classes/import.sql
+++
@@ -1,6 +0,0 @@
-
-INSERT INTO tb_users (name, age, email, city, cep)  VALUES ('Marcos', 25, 'marcos@gmail.com', 'Manaus', 69077769)
-INSERT INTO tb_users (name, age, email, city, cep)  VALUES ('Cícero', 30, 'cicero@hotmail.com', 'Faro', 6907787)
-INSERT INTO tb_users (name, age, email, city, cep)  VALUES ('Ket', 30, 'ket@gmail.com', 'São Paulo', 69140000)
-INSERT INTO tb_users (name, age, email, city, cep)  VALUES ('André', 20, 'andre@uol.com', 'Manaquiri', 69140452)
-INSERT INTO tb_users (name, age, email, city, cep)  VALUES ('Ramon', 35, 'ramon@gmail.com', 'Manaus', 65987891)
