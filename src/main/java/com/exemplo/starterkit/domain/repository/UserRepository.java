119a9e70695812af4373780c5453b2bf7767a500
marcosrebelo97 <mgr@icomp.ufam.edu.br>
2023-08-08 21:19:32 -0400

    Refatorando class UserRepository

--- src/main/java/com/exemplo/starterkit/domain/repository/UserRepository.java
+++ src/main/java/com/exemplo/starterkit/domain/repository/UserRepository.java
@@ -9,8 +9,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    public User getUserById(Long id);
-
     Optional<User> findByEmail(String email);
 
 }
