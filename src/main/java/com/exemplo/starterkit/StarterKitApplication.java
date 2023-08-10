460b2a9489a29c11515b0362c7060675f914fe0e
marcosrebelo97 <mgr@icomp.ufam.edu.br>
2023-08-10 11:23:12 -0400

    Documentando API via Swagger

--- src/main/java/com/exemplo/starterkit/StarterKitApplication.java
+++ src/main/java/com/exemplo/starterkit/StarterKitApplication.java
@@ -1,9 +1,13 @@
 package com.exemplo.starterkit;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info =
+@Info(title = "API REST Spring Boot Test", version = "1.0.0", description = "Exemplo de aplicação para testar conhecimentos"))
 public class StarterKitApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/exemplo/starterkit/api/controller/UserController.java
+++ src/main/java/com/exemplo/starterkit/api/controller/UserController.java
@@ -3,6 +3,8 @@ package com.exemplo.starterkit.api.controller;
 import com.exemplo.starterkit.api.dto.UserDTO;
 import com.exemplo.starterkit.domain.model.User;
 import com.exemplo.starterkit.domain.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +17,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping(value = "/users", produces = {"application/json"})
 public class UserController {
 
     @Autowired
@@ -28,30 +30,35 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Lista o todos os usuários cadastrados", method = "GET")
     @GetMapping
     public ResponseEntity<List<UserDTO>> list (){
         return ResponseEntity.ok().body(userService.list()
                 .stream().map(x -> modelMapper.map(x, UserDTO.class)).collect(Collectors.toList()));
     }
 
+    @Operation(summary = "Realiza cadastro de um novo usuário", method = "POST")
     @PostMapping
     public ResponseEntity<User> createUser (@RequestBody @Valid UserDTO userDTO){
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(userService.createUser(userDTO));
     }
 
+    @Operation(summary = "Realiza busca por usuário através do ID", method = "GET")
     @GetMapping("/{id}")
     public ResponseEntity<UserDTO> searchUserId (@PathVariable Long id){
         return ResponseEntity.ok().
                 body(modelMapper.map(userService.searchUserId(id), UserDTO.class));
     }
 
+    @Operation(summary = "Exclui usuário através do ID", method = "DELETE")
     @DeleteMapping("/{id}")
     public ResponseEntity<UserDTO> deleteUser(@PathVariable Long id){
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Atualiza dados de usuário cadastrado", method = "PUT")
     @PutMapping("/{id}")
     public ResponseEntity<UserDTO> update (@RequestBody @Valid UserDTO userDTO, @PathVariable Long id){
         return ResponseEntity.ok().body(modelMapper.map(userService.update(userDTO), UserDTO.class));
